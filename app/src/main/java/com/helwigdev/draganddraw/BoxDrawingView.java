74e028bc5f68d667b342874c576404d65493f7ff
helwig <helwig@susqu.edu>
2015-02-17 17:00:38 -0500

    2.17.15 Final

--- app/src/main/java/com/helwigdev/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/helwigdev/draganddraw/BoxDrawingView.java
@@ -1,11 +1,15 @@
 package com.helwigdev.draganddraw;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
 
 /**
  * Created by Tyler on 2/17/2015.
@@ -14,12 +18,38 @@ import android.view.View;
 public class BoxDrawingView extends View {
 	public static final String TAG = "BoxDrawingView";
 
+	private Box mCurrentBox;
+	private ArrayList<Box> mBoxes = new ArrayList<>();
+	private Paint mBoxPaint;
+	private Paint mBackgroundPaint;
+
 	public BoxDrawingView(Context context) {
 		this(context, null);
 	}
 
 	public BoxDrawingView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+
+		//paint the boxes semitransparent red
+		mBoxPaint = new Paint();
+		mBoxPaint.setColor(0x22ff0000);
+
+		mBackgroundPaint = new Paint();
+		mBackgroundPaint.setColor(0xfff8efe0);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.drawPaint(mBackgroundPaint);
+
+		for(Box box : mBoxes){
+			float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+			float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+			float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+			float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+
+			canvas.drawRect(left, top, right, bottom, mBoxPaint);
+		}
 	}
 
 	@Override
@@ -30,15 +60,23 @@ public class BoxDrawingView extends View {
 		switch (event.getAction()){
 			case MotionEvent.ACTION_DOWN:
 				Log.i(TAG, "  ACTION_DOWN");
+				mCurrentBox = new Box(curr);
+				mBoxes.add(mCurrentBox);
 				break;
 			case MotionEvent.ACTION_MOVE:
 				Log.i(TAG, "  ACTION_MOVE");
+				if(mCurrentBox != null){
+					mCurrentBox.setCurrent(curr);
+					invalidate();
+				}
 				break;
 			case MotionEvent.ACTION_UP:
 				Log.i(TAG, "  ACTION_UP");
+				mCurrentBox = null;
 				break;
 			case MotionEvent.ACTION_CANCEL:
 				Log.i(TAG, "  ACTION_CANCEL");
+				mCurrentBox = null;
 				break;
 		}
 		return true;
